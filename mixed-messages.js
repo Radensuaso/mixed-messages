8b414fe61180aa54d5d1f9401d7bde3f7200cb5d
Radensuaso <andrels9283@gmail.com>
2021-04-15 10:40:37 +0100

    basic structure of the code done

---
+++ mixed-messages.js
@@ -0,0 +1,21 @@
+const sentenceGenerator = {
+    subjects : [],
+    verbs : [],
+    objects : [],
+    prepositionalPhrase : [],
+    generateSubject () {
+
+    },
+    generateVerb () {
+
+    },
+    generateObject () {
+
+    },
+    generatePrepositionalPhrase () {
+
+    },
+    generateSentence () {
+
+    }
+}
