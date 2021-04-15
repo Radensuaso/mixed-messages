020531228b9d3d8fb673504e38c4257a2d592367
Radensuaso <andrels9283@gmail.com>
2021-04-15 15:18:11 +0100

    Formated mixed-messages.js with prettier extension. Added a README file

---
+++ README.md
@@ -0,0 +1,18 @@
+# Mixed Messages
+## Introduction
+This is a Project suggested in the Codecademy Full Stack Engineer path course. The aim of this project is mostly learn to work in a developer's environment and getting used to Git and Github. This app runs in the terminal and it's a very simple javascript app. Every time you run it, it generates a radom sentence. Each sentence is made up of four parts: subject, verb, object and prepositional phrase.
+
+## Technologies
+* Javascript
+* Command line
+* Node js
+
+## Launch
+
+To use this app it's best to have basic knowledge of the command line, and node js. Then you can download just the mixed-messages.js file. If you have windows you must have git Bash installed. For any OS download and install Node js.
+
+1. Open the terminal on the directory you have the file.
+2. Then just type: Node mixed-messages.js.
+3. You'll get a random message any time you type it. (quick tip: copy and paste it to get the sentences quicker.)
+
+If you feel adventurous you can change and/or add verbs, objects and prepositional phrases.
--- mixed-messages.js
+++ mixed-messages.js
@@ -1,39 +1,91 @@
 //I decided to make an object for this simple app
 const sentenceGenerator = {
-    //Added some arrays of the different parts of the random sentence.
-    //In the begining I thought to add the "It" subject, but it didn't go well in the different random sentences.
-    subjects : ["I", "You", "He", "She", "We", "They"],
-    //Feel free to add and/or replace the verbs, objects and prepositional phrases
-    //The app is developed to work with just verbs in the past
-    verbs : ["became", "dug", "baked", "found",
-    "felt", "fought", "forgot", "heard", "knew", "programmed",
-    "saw", "taught", "dressed", "dried", "eliminated",
-    "enjoyed", "followed", "freezed", "fried", "persuaded", 
-    "kissed", "invited", "loved"],
-    objects : ["the law", "the crime", "the computer",
-    "the robot", "the Mafia", "the gorila", "the chihuahua",
-    "the god of turnips", "the Batman", "the chipmunk",
-    "the race car", "the Hellhound", "the vampire",
-    "the Werewolf", "Johnny Bravo", "Bugs Bunny"],
-    prepositionalPhrases : ["by force", "by mistake",
-    "by no means", "at a moment's notice", "at night",
-    "for a price", "for a good cause", "for a reason",
-    "for a change", "for love", "for real", "in exchange for kiss",
-    "in fear of Goku", "on schedule", "on the record"],
-    //Down here is the function that randomly generates the sentencee
-    generate () {
-        /* Here are the variables that randomly store the different part of the sentence
-        coded the random subject, verb, object and preposional phrase to be as long as the arrays above */
-        const randomSubject = this.subjects[Math.floor(Math.random() * (this.subjects.length -1))];
-        const randomVerb = this.verbs[Math.floor(Math.random() * 
-        (this.verbs.length - 1))];
-        const randomObject = this.objects[Math.floor(Math.random() * 
-        (this.objects.length - 1))];
-        const randomPrepositionalPhrase = this.prepositionalPhrases[Math.floor(
-        Math.random() * (this.prepositionalPhrases.length - 1))];
-        //finally I just made the function to just print the random sentence to the console.
-        console.log(`${randomSubject} ${randomVerb} ${randomObject} ${randomPrepositionalPhrase}.`)
-    }
-}
+  //Added some arrays of the different parts of the random sentence.
+  //In the beginning I thought to add the "It" subject, but it didn't go well in the different random sentences.
+  subjects: ["I", "You", "He", "She", "We", "They"],
+  //Feel free to add and/or replace the verbs, objects and prepositional phrases
+  //The app is developed to work with just verbs in the past
+  verbs: [
+    "became",
+    "dug",
+    "baked",
+    "found",
+    "felt",
+    "fought",
+    "forgot",
+    "heard",
+    "knew",
+    "programmed",
+    "saw",
+    "taught",
+    "dressed",
+    "dried",
+    "eliminated",
+    "enjoyed",
+    "followed",
+    "freezed",
+    "fried",
+    "persuaded",
+    "kissed",
+    "invited",
+    "loved",
+  ],
+  objects: [
+    "the law",
+    "the crime",
+    "the computer",
+    "the robot",
+    "the Mafia",
+    "the gorilla",
+    "the chihuahua",
+    "the god of turnips",
+    "the Batman",
+    "the chipmunk",
+    "the race car",
+    "the Hellhound",
+    "the vampire",
+    "the Werewolf",
+    "Johnny Bravo",
+    "Bugs Bunny",
+  ],
+  prepositionalPhrases: [
+    "by force",
+    "by mistake",
+    "by no means",
+    "at a moment's notice",
+    "at night",
+    "for a price",
+    "for a good cause",
+    "for a reason",
+    "for a change",
+    "for love",
+    "for real",
+    "in exchange for kiss",
+    "in fear of Goku",
+    "on schedule",
+    "on the record",
+  ],
+  //Down here is the function that randomly generates the sentence
+  generate() {
+    /* Here are the variables that randomly store the different part of the sentence
+        coded the random subject, verb, object and prepositional phrase to be as long as the arrays above */
+    const randomSubject = this.subjects[
+      Math.floor(Math.random() * (this.subjects.length - 1))
+    ];
+    const randomVerb = this.verbs[
+      Math.floor(Math.random() * (this.verbs.length - 1))
+    ];
+    const randomObject = this.objects[
+      Math.floor(Math.random() * (this.objects.length - 1))
+    ];
+    const randomPrepositionalPhrase = this.prepositionalPhrases[
+      Math.floor(Math.random() * (this.prepositionalPhrases.length - 1))
+    ];
+    //finally I just made the function to just print the random sentence to the console.
+    console.log(
+      `${randomSubject} ${randomVerb} ${randomObject} ${randomPrepositionalPhrase}.`
+    );
+  },
+};
 
 sentenceGenerator.generate();
