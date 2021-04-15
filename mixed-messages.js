38c808c0364625481b309a0320fd57ae57ee87ae
Radensuaso <andrels9283@gmail.com>
2021-04-15 11:00:07 +0100

    Added the subjects and verbs

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,6 +1,10 @@
 const sentenceGenerator = {
-    subjects : [],
-    verbs : [],
+    subjects : ["I", "You", "It", "He", "She", "We", "They"],
+    verbs : ["was", "became", "dug", "did", "found",
+    "felt", "fought", "forgot", "heard", "knew", "made",
+    "saw", "taught", "dressed", "dried", "eliminated",
+    "enjoyed", "followed", "freezed", "fried", "persuaded", 
+    "kissed", "invited"],
     objects : [],
     prepositionalPhrase : [],
     generateSubject () {
