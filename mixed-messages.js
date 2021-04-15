2a3df97f66bf601c261785bea4fe9cf6cc21e5b6
Radensuaso <andrels9283@gmail.com>
2021-04-15 11:32:55 +0100

    Took out was verb, added loved verb. Added objects.

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,11 +1,15 @@
 const sentenceGenerator = {
     subjects : ["I", "You", "It", "He", "She", "We", "They"],
-    verbs : ["was", "became", "dug", "did", "found",
+    verbs : ["became", "dug", "did", "found",
     "felt", "fought", "forgot", "heard", "knew", "made",
     "saw", "taught", "dressed", "dried", "eliminated",
     "enjoyed", "followed", "freezed", "fried", "persuaded", 
-    "kissed", "invited"],
-    objects : [],
+    "kissed", "invited", "loved"],
+    objects : ["the law", "the crime", "the computer",
+    "the robot", "the medicine", "the gorila", "the chihuahua",
+    "the god of turnips", "the Batman", "the chipmunk",
+    "the race car", "the Hellhound", "the vampire",
+    "the Werewolf", "Johnny Bravo", "Bugs Bunny"],
     prepositionalPhrase : [],
     generateSubject () {
 
